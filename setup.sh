e40e8a84494e899cb1a0345200cf09ce21b60ce0
Wei Ren <6765243+weiren2@users.noreply.github.com>
2022-08-12 00:39:53 -0500

    Refactor omz omt (#1)
    * Running `setup.sh` again will pull updates from oh-my-tmux upstream repo.
    
    * Include customization for oh-my-zsh.
    
    * Added zsh-syntax-highlighting in omz.
    
    * Add syntax highlight and autosuggest.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
-
 CURR_DIR=$(pwd -P)
-NVIM_EXISTS=no
 
+###### Utility Functions ######
+function download_or_update_repo {
+	local destination_dir=$1
+	local git_repo_url=$2
+	if [[ ! -d $destination_dir ]]; then
+		git clone $git_repo_url $destination_dir
+	else
+		git -C $destination_dir pull
+	fi
+}
+
+###### Neovim & Vim ######
+NVIM_EXISTS=no
 # Check if nvim exists
 if nvim_loc="$(type -p nvim)" && [[ -n $nvim_loc ]]; then
 	NVIM_EXISTS=yes
@@ -34,6 +45,7 @@ else
 	echo "Neovim does not exist! Skipping..."
 fi
 
+###### Oh My Zsh & Config ######
 echo
 # Install oh-my-zsh for zsh
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
@@ -42,18 +54,37 @@ else
 	echo "Oh-my-zsh already installed!"
 fi
 
+OMZ_CUSTOM_PLUGIN_DIR="$CURR_DIR/omz-custom/plugins"
+
+ZSH_SYNTAX_HIGHLIGHT_DIR="$OMZ_CUSTOM_PLUGIN_DIR/zsh-syntax-highlighting"
+ZSH_SYNTAX_HIGHLIGHT_GIT_REPO="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+download_or_update_repo $ZSH_SYNTAX_HIGHLIGHT_DIR $ZSH_SYNTAX_HIGHLIGHT_GIT_REPO
+
+ZSH_AUTOSUGGEST_DIR="$OMZ_CUSTOM_PLUGIN_DIR/zsh-autosuggestions"
+ZSH_AUTOSUGGEST_GIT_REPO="https://github.com/zsh-users/zsh-autosuggestions"
+download_or_update_repo $ZSH_AUTOSUGGEST_DIR $ZSH_AUTOSUGGEST_GIT_REPO
+
 # Use customized zsh / oh-my-zsh config
 ln -sfn $CURR_DIR/.zshrc $HOME/.zshrc
 
 
+###### Oh My Tmux config ######
 echo
-# Oh My Tmux config
-if [[ ! -d "$HOME/.tmux" ]]; then
+TMUX_OMT_DIR="$HOME/.tmux"
+if [[ ! -d $TMUX_OMT_DIR ]]; then
 	cd ~
 	git clone https://github.com/gpakosz/.tmux.git || true
 	ln -s -f .tmux/.tmux.conf
-	cp .tmux/.tmux.conf.local .
-  echo "Oh-my-tmux configuration installed in $HOME/.tmux!"
+  echo "Oh-my-tmux configuration installed in $TMUX_OMT_DIR!"
 else
-	echo "Oh-my-tmux already installed in $HOME/.tmux!"
+	echo "Oh-my-tmux already installed in $TMUX_OMT_DIR! Updating it now."
+	git -C $TMUX_OMT_DIR pull
+	# Oh My Tmux provides some sane defaults. Update local config as well.
 fi
+
+# Update local Oh My Tmux local config. Backup previous one if necessary.
+TMUX_OMT_LOCAL_CONF="$HOME/.tmux.conf.local"
+if [[ -f $TMUX_OMT_LOCAL_CONF ]]; then
+	mv "$TMUX_OMT_LOCAL_CONF" "$TMUX_OMT_LOCAL_CONF.backup"
+fi
+cp $TMUX_OMT_DIR/.tmux.conf.local $TMUX_OMT_LOCAL_CONF
